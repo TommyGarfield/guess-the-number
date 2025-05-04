c863e64da3c34db294e08b30e424c5bc695d4b78
Thomas <163894641+TommyGarfield@users.noreply.github.com>
2025-05-04 16:35:07 +0200

    Add files via upload
    added guess comparison

--- guess_the_number.py
+++ guess_the_number.py
@@ -4,6 +4,13 @@ secret_number = random.randint(1, 20)
 
 guess = int(input("Try to guess the secret number... "))
 
+if guess == secret_number:
+    print("Congratulations!!!")
+elif guess < secret_number:
+    print("Too low, try again")
+elif guess > secret_number:
+    print("Too high, try again")
 
-print(guess)
+
+#print(guess)
 #print(secret_number)
