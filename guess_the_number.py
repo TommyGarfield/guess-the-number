891c7f8b1c6e1bb8647915a7636ed4ad11ff860a
Thomas <163894641+TommyGarfield@users.noreply.github.com>
2025-05-04 16:48:47 +0200

    Completed game loop.

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,9 +1,12 @@
 import random
 
+#storing random number in secret_number
 secret_number = random.randint(1, 20)
 
+#asks user for number
 guess = int(input("Try to guess the secret number... "))
 
+#if number too high or low ask them to try again
 while guess != secret_number:
     if guess < secret_number:
         print("Too low!")
@@ -13,7 +16,9 @@ while guess != secret_number:
         guess = int(input("Try again... "))
     
 
-if guess == secret_number:
-        print("Congratulations!!!")
+#prints message when guess is correct
+print("Congratulations, thats the number!!!")
+
+        
 #print(guess)
 #print(secret_number)
