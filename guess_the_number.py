db18a7440dcfb52f76e613ec42cc71cd628c4c2f
Thomas <163894641+TommyGarfield@users.noreply.github.com>
2025-05-04 17:44:43 +0200

    added counter to measure attempts

--- guess_the_number.py
+++ guess_the_number.py
@@ -3,22 +3,28 @@ import random
 #storing random number in secret_number
 secret_number = random.randint(1, 20)
 
+#created a guess counter
+counter = 0
+
 #asks user for number
 guess = int(input("Try to guess the secret number... "))
+counter += 1
 
-#if number too high or low ask them to try again
+
+#if number too high or low ask them to try again,
+#adds 1 to the guess counter each time
 while guess != secret_number:
     if guess < secret_number:
         print("Too low!")
-        guess = int(input("Try again... "))
+       
     elif guess > secret_number:
         print("Too high")
-        guess = int(input("Try again... "))
+
+    guess = int(input("Try again... "))
+    counter += 1
     
+        
 
 #prints message when guess is correct
-print("Congratulations, thats the number!!!")
+print("Congratulations, thats the number!!! It took you",counter, "guesses!")
 
-        
-#print(guess)
-#print(secret_number)
